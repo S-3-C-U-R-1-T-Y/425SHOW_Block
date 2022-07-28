54f8d6e91f5dd0a44bd9c1cf18e2bfb9a40d4cbb
S3CUR1TY <30468513+S-3-C-U-R-1-T-Y@users.noreply.github.com>
2022-07-28 13:41:04 +0200

    Create demo.go

---
+++ demo.go
@@ -0,0 +1,4 @@
+"AadClientId": "80a34b62-2bac-43e4-93af-66124ad0f3ff",
+"AadSecret": "Ky28Q~W90jenYoYtXnIcGEcU_iLWZM.SY1bPJa68",
+"AadTenantDomain": "nedscaperlab.onmicrosoft.com",
+"AadTenantId": "316bec54-8d14-4882-8af3-ee5e61570603"
