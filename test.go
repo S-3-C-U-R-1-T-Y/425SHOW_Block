79d260d67898c33dbc75f1c267829e609aa20c2e
S3CUR1TY <30468513+S-3-C-U-R-1-T-Y@users.noreply.github.com>
2022-08-06 08:26:43 +0200

    Create test.go

---
+++ test.go
@@ -0,0 +1,4 @@
+"AadClientId": "80a34b62-2bac-43e4-93af-66124ad0f3ff",
+"AadSecret": "GAx8Q~OLuhYEJkr6~xGhWtxeo5TM8PUFqA_lYc-G",
+"AadTenantDomain": "nedscaperlab.onmicrosoft.com",
+"AadTenantId": "316bec54-8d14-4882-8af3-ee5e61570603"
